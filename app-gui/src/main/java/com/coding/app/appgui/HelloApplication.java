3c3220c808c008fd14e7858a2ad2245b620323eb
Rdjaouzi <riadh.djaouzi@gmail.com>
2024-11-28 09:33:38 +0100

    app gui

--- app-gui/src/main/java/com/coding/app/appgui/HelloApplication.java
+++ app-gui/src/main/java/com/coding/app/appgui/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view-riadh.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
         stage.centerOnScreen(); // Centre the window on the screen
         stage.setTitle("Hello!");
