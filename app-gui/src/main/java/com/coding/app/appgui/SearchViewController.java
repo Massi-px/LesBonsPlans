f39957c63f8f9dcee6c40c2f1187c4d8f9a6a6fd
Massimiliano Pinna <maximiliano.pinna@gmail.com>
2024-11-29 14:26:54 +0100

    Change SearView Style

--- app-gui/src/main/java/com/coding/app/appgui/SearchViewController.java
+++ app-gui/src/main/java/com/coding/app/appgui/SearchViewController.java
@@ -9,11 +9,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import org.controlsfx.control.CheckComboBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 
 import java.util.List;
 
@@ -26,14 +26,22 @@ public class SearchViewController {
     private TextField refreshFrequencyField;
     @FXML
     private ListView<Annonce> listingsListView;
+    @FXML
+    private Button startSearchButton;
+    @FXML
+    private Button stopSearchButton;
+    @FXML
+    private Region spacer;
 
     private final Dispatcher dispatcher = new Dispatcher();
 
     ObservableList<Annonce> observabled = FXCollections.observableArrayList();
+    private final AnnonceDao annonceDao = new AnnonceDao();
 
 
     @FXML
     public void initialize() {
+        HBox.setHgrow(spacer, Priority.ALWAYS);
         siteCheckComboBox.getItems().addAll("Les Bons Plans", "Le Bon Coin");
         listingsListView.setItems( observabled );
 
@@ -47,6 +55,27 @@ public class SearchViewController {
         listingsListView.setContextMenu(contextMenu);
         // Set action event handler for menuItem1
         menuItem.setOnAction(event -> handleSaveAction());
+    }
+
+    @FXML
+    private void increaseRefreshFrequency() {
+        int currentValue = getRefreshFrequency();
+        refreshFrequencyField.setText(String.valueOf(currentValue + 1));
+    }
+
+    @FXML
+    private void decreaseRefreshFrequency() {
+        int currentValue = getRefreshFrequency();
+        if (currentValue > 0) {
+            refreshFrequencyField.setText(String.valueOf(currentValue - 1));
+        }
+    }
+
+    @FXML
+    protected void onStopSearchClick() {
+        startSearchButton.setDisable(false);
+        stopSearchButton.setDisable(true);
+        // Add logic to stop the search if needed
     }
 
     @FXML
@@ -65,18 +94,26 @@ public class SearchViewController {
             }
         }
     }
+
     @FXML
     protected void onClearSearchClick() {
         observabled.clear();
     }
-
-    private final AnnonceDao annonceDao = new AnnonceDao();
 
     @FXML
     private void onSaveSelectedClick() {
         List<Annonce> selectedAnnonces = listingsListView.getSelectionModel().getSelectedItems();
         for (Annonce annonce : selectedAnnonces) {
             annonceDao.saveAnnonce( annonce );
+        }
+    }
+
+
+    private int getRefreshFrequency() {
+        try {
+            return Integer.parseInt(refreshFrequencyField.getText());
+        } catch (NumberFormatException e) {
+            return 0;
         }
     }
 
